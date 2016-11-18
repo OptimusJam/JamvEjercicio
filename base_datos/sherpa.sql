03231e492e6260824b5d724097cd7cfadad7f98a
OptimusJA <myemotion.mail@gmail.com>
2016-11-18 10:28:31 +0100

    Estructura de la base de datos

---
+++ base_datos/sherpa.sql
@@ -0,0 +1,53 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 18-11-2016 a las 10:25:35
+-- Versión del servidor: 5.5.25a
+-- Versión de PHP: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `sherpa`
+--
+CREATE DATABASE IF NOT EXISTS `sherpa` DEFAULT CHARACTER SET latin1 COLLATE latin1_spanish_ci;
+USE `sherpa`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle`
+--
+
+CREATE TABLE IF NOT EXISTS `detalle` (
+  `id_detalle` int(11) NOT NULL AUTO_INCREMENT,
+  `codigo_postal` int(11) NOT NULL,
+  `ciudad` varchar(100) COLLATE latin1_spanish_ci NOT NULL,
+  `id_master` int(11) NOT NULL,
+  PRIMARY KEY (`id_detalle`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci AUTO_INCREMENT=8 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `master`
+--
+
+CREATE TABLE IF NOT EXISTS `master` (
+  `id_master` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
+  PRIMARY KEY (`id_master`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci AUTO_INCREMENT=7 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
