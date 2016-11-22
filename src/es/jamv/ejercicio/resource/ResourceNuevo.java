71d0babb3820ec3a3f5c4af2fb898092bd332ea9
OptimusJA <myemotion.mail@gmail.com>
2016-11-22 14:40:46 +0100

    Nuevo recurso

---
+++ src/es/jamv/ejercicio/resource/ResourceNuevo.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package es.jamv.ejercicio.resource;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import es.jamv.ejercicio.logicanegocio.FactoriaLogicaNegocio;
+import es.jamv.ejercicio.logicanegocio.InterfaceLogicaNegocio;
+import es.jamv.ejercicio.modelo.Modelo;
+
+/**
+ * @author joseangel.martinez
+ *
+ */
+@Path("/Nuevo/")
+public class ResourceNuevo {
+
+	@GET
+	@Path("/getCiudad/{nombre}/{codigoPostal}")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
+    public Modelo getCiudad(@NotNull @PathParam("nombre") final String nombre, 
+    						@NotNull @Digits(integer=5, fraction=0) @PathParam("codigoPostal") int codigoPostal) {
+		
+		Modelo objModelo = new Modelo();
+		
+		//Se accede a la lógica de negocio
+		InterfaceLogicaNegocio objLogicaNegocio = new FactoriaLogicaNegocio().obtenerLogicaNegocio();
+		
+		objModelo = objLogicaNegocio.realizaProceso(nombre, codigoPostal);
+		
+        return objModelo;
+    }
+	
+}
